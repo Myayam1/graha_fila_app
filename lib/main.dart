bd6a5c91468c6c0704c94fea6b01bb8468d5b7db
KaIkaf <caesarkai872@gmail.com>
2025-04-10 11:40:26 +0700

    Feat : [CK] menambahkan page saat data kosong untuk db reservasi

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:grafil_app/pages/dashboard/dashboardlapangan.dart';
 import 'package:grafil_app/pages/dashboard/dashboardreservasipage.dart';
+import 'package:grafil_app/pages/dashboard/dataisemptydbreservasi.dart';
 import 'package:grafil_app/pages/riwayat/pageriwayatreservasi.dart';
 import 'package:grafil_app/register/registerpage.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        home: RegisterPage(),
+        home: DashboardReservasiEmptyPage(),
     );
   }
 }
